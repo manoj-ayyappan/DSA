562e5f53192e856e3d54d81821952f9ff5f1ac06
mayyapp <mayyapp@ncsu.edu>
2023-05-24 10:07:20 -0400

    added aaray 1 file

---
+++ array-1.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main() {
+    // declare an array
+    int A[5];
+    // declare and initialize array
+    int B[5] = {2, 4, 6, 8, 10};
+    // set value
+    A[0] = 0;
+
+    // initialize array with all 0's
+    int C[5] = {0};
+
+    // printf("%d", A[0]);
+
+    for(int i=0; i<5; i++) {
+        printf("%d\n", C[i]);
+    }
+
+    return 0;
+}
