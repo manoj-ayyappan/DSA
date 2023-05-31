454dd0fa5b99371fc5ff2ae51b423510335e637d
mayyapp <manojayyappan18@gmail.com>
2023-05-30 21:26:14 -0400

    Modified array as parameter files

--- array-as-parameter.cpp
+++ array-as-parameter.cpp
@@ -4,6 +4,10 @@
 
 void fun(int A[], int n) {
     int i;
+
+    // for each loop cannot be used with pointer
+    // (for each loop is only available in C++)
+    
     for(i=0;i<n;i++)
         printf("%d\n", A[i]);
 }
