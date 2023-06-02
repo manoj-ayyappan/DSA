c7b453a8711d71b2b94d97857acc7e6e66428daf
mayyapp <manojayyappan18@gmail.com>
2023-06-01 23:11:31 -0400

    Added constructer code

--- c-to-cpp.cpp
+++ c-to-cpp.cpp
@@ -10,7 +10,8 @@ class Rectangle
     int breadth;
 
     public:
-    void initialize(int l, int b) {
+    // void initialize(int l, int b) {
+    Rectangle(int l, int b) {           // constructor
         length = l;
         breadth = b;
     }
@@ -26,8 +27,8 @@ class Rectangle
 };
 
 int main() {
-    struct Rectangle r;
-    r.initialize(10, 5);
+    Rectangle r(10,5);
+    // r.initialize(10, 5);
     int a = r.area();
     r.changelength(20);
 
