029d453b1173c976d8e1dedea25e238281f991e2
mayyapp <manojayyappan18@gmail.com>
2023-06-11 18:12:11 -0400

    Added Rcursion folder

---
+++ Recursion/tailhead.cpp
@@ -0,0 +1,26 @@
+// coding recursion 1
+
+#include<stdio.h>
+
+void fun1(int n) {
+    if(n>0) {
+        // tail recursion
+        printf("%d ", n);
+        fun1(n-1);
+    }
+}
+
+void fun2(int n) {
+    if(n>0) {
+        // head recursion
+        fun2(n-1);
+        printf("%d ", n);
+    }
+}
+
+int main() {
+    int x = 3;
+    fun1(x);
+    fun2(x);
+    return 0;
+}
