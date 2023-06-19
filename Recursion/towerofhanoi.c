9166a662e6a875454c24499bcf52e7fb8b2cfca5
mayyapp <manojayyappan18@gmail.com>
2023-06-19 11:06:29 -0400

    Added Tower of hanoi files

---
+++ Recursion/towerofhanoi.c
@@ -0,0 +1,16 @@
+// Tower of Hanoi problem using recursion
+
+#include<stdio.h>
+
+void TOH(int n, int A, int B, int C) {          // O(2^n)
+    if(n>0) {
+        TOH(n-1, A, C, B);
+        printf("(%d, %d) ", A, C);
+        TOH(n-1, B, A, C);
+    }
+}
+
+int main() {
+    TOH(3, 1, 2, 3);
+    return 0;
+}
