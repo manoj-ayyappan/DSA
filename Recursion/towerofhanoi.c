202365165292028f44679b515dcfe9ab332e8784
mayyapp <manojayyappan18@gmail.com>
2023-06-20 22:41:31 -0400

    Comment

--- Recursion/towerofhanoi.c
+++ Recursion/towerofhanoi.c
@@ -4,6 +4,7 @@
 
 void TOH(int n, int A, int B, int C) {          // O(2^n)
     if(n>0) {
+        // move plate from Ato B using C
         TOH(n-1, A, C, B);
         printf("(%d, %d) ", A, C);
         TOH(n-1, B, A, C);
