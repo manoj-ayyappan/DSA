af281b4f6c60552f7f9f9c24871fd3afd4fde19d
mayyapp <manojayyappan18@gmail.com>
2023-06-24 10:47:56 -0400

    Increase array size file

---
+++ Arrays/increase-array-size.cpp
@@ -0,0 +1,32 @@
+// how to increase array size
+// you cannot directly do so
+// there is one way to do it in heap memory
+
+#include<stdio.h>
+
+
+int main() {
+    // initial array of size 5
+    int *p = new int[5];
+    p[0] = 1; p[1] = 2; p[2] = 3; p[3] = 4; p[4] = 5;
+
+    // new array of size 10
+    int *q = new int[10];
+
+    // transfer data
+    for (int i = 0; i < 5; i++)
+    {
+        q[i] = p[i];
+    }
+
+    // delete og array
+    delete []p;
+
+    // set p
+    p = q;
+
+    // remove q
+    q = NULL;
+
+    return 0;
+}
