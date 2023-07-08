054f17350129c690659c7f18301d7ff7c6deb68f
mayyapp <manojayyappan18@gmail.com>
2023-07-08 15:00:39 -0400

    Modified array adt file

--- Array-ADT/start.c
+++ Array-ADT/start.c
@@ -6,7 +6,10 @@
 
 struct Array
 {
-    int *A;
+    // Use this code to create an array in the heap memory
+    // int *A;
+
+    int A[10];
     int size;
     int length;
 };
@@ -14,26 +17,30 @@ struct Array
 void Display(struct Array arr) {
     int i;
     printf("\nElements are\n");
-    for(i=0; i<arr.length; i++) {
+    for(i=0; i<arr.length; i++)
         printf("%d ", arr.A[i]);
-    }
 }
 
 int main() {
-    struct Array arr;
-    int n, i;
-    printf("Enter the array size: ");
-    scanf("%d", &arr.size);
-    arr.A = (int *)malloc(arr.size*sizeof(int));
-    arr.length = 0;
 
-    printf("Enter the number of numbers\n");
-    scanf("%d", &n);
+    struct Array arr = {{2,3,4,5,6}, 10, 5};
 
-    printf("Enter all the elements\n");
-    for(i=0; i<n; i++)
-        scanf("%d", &arr.A[i]);
-    arr.length = n;
+    // Use this code to create an array in the heap memory
+
+    // struct Array arr;
+    // int n, i;
+    // printf("Enter the array size: ");
+    // scanf("%d", &arr.size);
+    // arr.A = (int *)malloc(arr.size*sizeof(int));
+    // arr.length = 0;
+
+    // printf("Enter the number of numbers\n");
+    // scanf("%d", &n);
+
+    // printf("Enter all the elements\n");
+    // for(i=0; i<n; i++)
+    //     scanf("%d", &arr.A[i]);
+    // arr.length = n;
 
     Display(arr);
 
