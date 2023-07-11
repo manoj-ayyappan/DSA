c10f778fb6017b53409283cc0d86450c50af9b87
mayyapp <manojayyappan18@gmail.com>
2023-07-10 22:47:52 -0400

    Added recursive binary search

--- Array-ADT/start.c
+++ Array-ADT/start.c
@@ -83,6 +83,26 @@ int LinearSearch(struct Array *arr, int key) {
     return -1; 
 }
 
+// Iterative Binary search
+int BinarySearch(struct Array arr, int key) {
+    int l, mid, h;
+    l = 0;
+    h = arr.length-1;
+
+    while(l<=h) {
+        mid = (l+h)/2;
+        if(key == arr.A[mid])
+            return mid;
+        else if(key < arr.A[mid])
+            h = mid - 1;
+        else
+            l = mid + 1;
+    }
+    return -1;
+}
+
+// 
+
 int main() {
 
     struct Array arr = {{2,3,4,5,6}, 10, 5};
@@ -108,7 +128,8 @@ int main() {
     // Append(&arr, 10);
     // Insert(&arr, 5, 11);
     // printf("%d\n", Delete(&arr, 2));
-    printf("%d\n", LinearSearch(&arr, 4));
+    // printf("%d\n", LinearSearch(&arr, 4));
+    printf("%d\n", BinarySearch(arr, 5));
     Display(arr);
 
     return 0;
